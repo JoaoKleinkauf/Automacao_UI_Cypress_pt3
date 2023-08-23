6dcadad08e662288634b319ed14da48353175133
JoaoKleinkauf <jprkleinkauf@gmail.com>
2023-08-22 22:31:11 -0300

    Teste checkout

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,8 +1,1 @@
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-// Cypress.Commands.add('cadastro', (usuario, senha) => {
-//     cy.get('.icon-user-unfollow').click()
-//     cy.get('#reg_email').type(usuario)
-//     cy.get('#reg_password').type(senha)
-//     cy.get(':nth-child(4) > .button').click()
-//  })
+/// <reference types='cypress' />
