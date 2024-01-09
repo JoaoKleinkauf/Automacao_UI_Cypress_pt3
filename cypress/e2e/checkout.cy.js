98a1f52dd83e51d917525cac6d0546384d04412d
JoaoKleinkauf <jprkleinkauf@gmail.com>
2024-01-08 23:33:34 -0300

    teste 3.2

--- cypress/e2e/checkout.cy.js
+++ cypress/e2e/checkout.cy.js
@@ -3,7 +3,11 @@
 const data = require('../fixtures/data.json')
 
 describe('Funcionalidade de Checkout', () => {    
-    it('Login com AppActions', () => {
+    beforeEach('Login com AppActions', () => {
         cy.login(data.usuario, data.senha)
     });
+
+    it('Checkout bem sucedido', () => {
+        cy.get('[aria-haspopup="true"] > .display-name').should('contain', 'gerente')
+    })
 });
