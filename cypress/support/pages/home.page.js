39ca399edcb83afbc2174651f57a24c0142c8f4e
JoaoKleinkauf <jprkleinkauf@gmail.com>
2024-02-22 21:55:07 -0300

    Teste intercept 2.0

--- cypress/support/pages/home.page.js
+++ cypress/support/pages/home.page.js
@@ -2,11 +2,21 @@
 
 class carrinho {
 
-    addCarrinho(){
+    addCarrinho() {
         cy.get('.button-variable-item-XS').click()
         cy.get('.button-variable-item-Orange').click()
         cy.get('.input-text').clear().type(1)
         cy.get('.single_add_to_cart_button').click()
+    }
+
+    get #email() { return cy.get("#reg_email") }
+    get #pass() { return cy.get("#reg_password") }
+    get #reg() {return cy.get(":nth-child(4) > .button")}
+
+    loginCar(emailFaker, passFaker) {
+        this.#email.type(emailFaker)
+        this.#pass.type(passFaker)
+        this.#reg.click()
     }
 }
 
